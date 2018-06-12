398e0cd56ae9c9328f4fb767cfbfa4da0ff44e63
austin callaghan <adcallaghan159@gmail.com>
2018-06-11 20:13:51 -0700

    this is just a test

--- assessment.js
+++ assessment.js
@@ -1,6 +1,6 @@
 // #1 create a variable called 'myName' and assign it to your name as a string
 
-// CODE HERE
+// CODE HERE :
 
 // #2 create a variable called 'faveNum' and assign it to your favorite number
 
